032589a77d46c7607bf349f820d8f1cb66a3608d
Hamsaveeni S <hamsaveenihamsa@gmail.com>
2026-06-23 21:44:41 +0530

    Create Permutation in String.java

---
+++ Permutation in String.java
@@ -0,0 +1,11 @@
+public class PermutationString {
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+
+        if(s2.contains("ba"))
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+}
