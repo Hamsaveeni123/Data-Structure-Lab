b7a1fe06df8d8ae9f8fef6b94cbfc715fc67f779
Hamsaveeni S <hamsaveenihamsa@gmail.com>
2026-06-25 19:38:01 +0530

    Create Search in Rotated Sorted Array.java

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,16 @@
+public class SearchRotatedArray {
+    public static void main(String[] args) {
+
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                System.out.println("Index = "+i);
+                return;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+}
