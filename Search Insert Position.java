e049d0efba36df8e9f03cc4538c3d20b6fcf4a7d
Hamsaveeni S <hamsaveenihamsa@gmail.com>
2026-06-25 19:37:14 +0530

    Create Search Insert Position.java

---
+++ Search Insert Position.java
@@ -0,0 +1,18 @@
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+
+        int[] arr = {1,3,5,6};
+        int target = 2;
+
+        int i;
+
+        for(i=0;i<arr.length;i++){
+            if(arr[i]>=target){
+                System.out.println(i);
+                return;
+            }
+        }
+
+        System.out.println(arr.length);
+    }
+}
