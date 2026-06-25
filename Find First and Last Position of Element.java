09da8d779c2bb54a004ab59f66d8a0d52583559d
Hamsaveeni S <hamsaveenihamsa@gmail.com>
2026-06-25 19:39:51 +0530

    Create Find First and Last Position of Element.java

---
+++ Find First and Last Position of Element.java
@@ -0,0 +1,20 @@
+public class FirstLastPosition {
+    public static void main(String[] args) {
+
+        int[] arr = {5,7,7,8,8,10};
+        int target = 8;
+
+        int first=-1,last=-1;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                if(first==-1)
+                    first=i;
+                last=i;
+            }
+        }
+
+        System.out.println("First = "+first);
+        System.out.println("Last = "+last);
+    }
+}
